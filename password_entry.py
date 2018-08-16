924fa489aadba368a8d9bf9574efaa709130dc75
David-Roy-JCU <david.roy@my.jcu.edu.au>
2018-08-16 14:25:44 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,12 @@
 """
 David Roy
 """
+
+MIN_LENGTH = 8
+
+password = input("Enter password (min {} characters): ".format(MIN_LENGTH))
+while len(password) < MIN_LENGTH:
+    print("Password must be {} characters".format(MIN_LENGTH))
+    password = input("Enter password (min {} characters): ".format(MIN_LENGTH))
+print()
+print("*" * len(password))
